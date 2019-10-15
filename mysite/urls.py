a53832285ffd478535b4d067988340fb6f53cfdb
dltjsdk907 <dltjsdsk907@nate.com>
2019-10-15 11:55:58 +0900

    Modified templates to display posts from database.

--- mysite/urls.py
+++ mysite/urls.py
@@ -8,10 +8,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views                           # !!!
-
+#
 urlpatterns = [
-    path('', views.homepage, name='home'),    # !!!
+    # path('', views.homepage, name='home'),    # !!!
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls')),
-    # path('', include('blog.urls')), # 마지막에 장고걸스랑 동일하게 하려고 수정한 부분
+    path('', include('blog.urls')), # 마지막에 장고걸스랑 동일하게 하려고 수정한 부분
 ]
